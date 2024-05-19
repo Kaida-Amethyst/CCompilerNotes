48f3f82879c72d1a579929afa04db627a851a74d
Kaida-Amethyst <ziyue.liu.vm@gmail.com>
2024-05-19 18:32:58 +0800

    change fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,8 @@
 import starlightPlugin from '@astrojs/starlight-tailwind';
 import colors from 'tailwindcss/colors';
 
-
-const accent = { 200: '#dac4b2', 600: '#975c22', 900: '#452c14', 950: '#302012' };
-const gray = { 100: '#eef8ff', 200: '#ddf1ff', 300: '#9fc8e4', 400: '#3695cb', 500: '#005f8a', 700: '#003d5a', 800: '#002a40', 900: '#001b2b' };
+const accent = { 200: '#cfbdfb', 600: '#8424ea', 900: '#3c1a6a', 950: '#2a1748' };
+const gray = { 100: '#f5f6f9', 200: '#eaeef3', 300: '#bec2c8', 400: '#848c97', 500: '#515963', 700: '#323942', 800: '#212730', 900: '#15181d' };
 
 
 /** @type {import('tailwindcss').Config} */
